699f7a7955e7b900f7849de2a96d2bd4e1e2f2e5
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-02-05 20:24:17 +0530

    EOD 05FEB

--- Object Oriented Programming/firstclass.py
+++ Object Oriented Programming/firstclass.py
@@ -1,10 +1,15 @@
 class Name:
+   
     def __init__(self,first_name, last_name):
         self.fname = first_name
-        self.lname = last_name 
+        self.lname = last_name
         self.get_name()
     
     def get_name(self):
+        '''
+        This Function Prints your name
+        '''
         print(f"Your name is {self.fname} {self.lname}")
 
+print("Hellooo")
 X = Name("Kaustubh", "Ajgaonkar")
