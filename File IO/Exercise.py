df8e1b13f286cd0c889ada67d25eff52d20e8872
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-03-02 21:37:41 +0530

    EOD2MARCH23

--- File IO/Exercise.py
+++ File IO/Exercise.py
@@ -1,22 +1,24 @@
+from translate import Translator
 # Program to Translate a file and its contents to Japanese
-file_name = input('Enter the file name that you want to trasnslate: ')
+file_name = input('Enter the file name that you want to translate: ')
 final_file = input('Enter the target file name: ')
 
+# Translation stuff
+japanese = Translator(to_lang="ja")
+
+# Opening an existing file
 try:
     with open(file_name, mode='r') as f1:
-        text = f1.read()
+        text = f1.read()  # Save read contents into this variable
+        with open(final_file, mode='a', encoding="utf-8") as f2:
+            trans_japan = japanese.translate(text)
+            f2.write(trans_japan)  # Write the translated into a new file
+            print('Write Operation completed')
 except FileNotFoundError as err:
     print('File is not found')
 except IOError as err:
     print(err)
 
-try:
-    with open(final_file, mode='w') as f2:
-        f2.write(text)
-except FileNotFoundError as err:
-    print('File not found')
-except IOError as err:
-    print('IO Error')
 
 
 
