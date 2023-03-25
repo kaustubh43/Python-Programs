cadb1053586b3d1c5e14ca6f1313757f781fefbf
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-03-25 20:54:16 +0530

    EOD25MAR

--- Function Programming/Dictionary Comprehension.py
+++ Function Programming/Dictionary Comprehension.py
@@ -6,7 +6,7 @@ simple_dict = {
 my_dict = {key: value**2 for key, value in simple_dict.items()}
 print(my_dict)
 
-my_dict2 = {key: value**2 for key, value in simple_dict.items() if value % 2 == 0} #Stores only the odd values of keys
+my_dict2 = {key: value**2 for key, value in simple_dict.items() if value % 2 == 0}  # Stores only the odd values of keys
 print(my_dict2)
 
 my_dict3 = {num: num * 2 for num in [1, 2, 3, 4, 5]}
