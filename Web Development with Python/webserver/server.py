9e0e4a39688270a676f0701f43b3e793aab793ca
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-04-08 21:36:09 +0530

    webdev2

--- Web Development with Python/webserver/server.py
+++ Web Development with Python/webserver/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__)
@@ -6,5 +6,21 @@ print(__name__)
 
 @app.route('/')
 def hello_world():
-    return"<p>Hello, World! Kaustubh Ajgaonkar!!!</p>"
+    return render_template('./index.html')
+
+
+@app.route('/blog')
+def blog():
+    return "<p>These are my thoughts on blog</p>"
+
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return "<p>These are my dogs. a blog post about dogs</p>"
+
+
+@app.route('/about.html')
+def about():
+    return render_template('./about.html')
+
 
