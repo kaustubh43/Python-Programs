f907e601009c99fee7dc72985d38052255b89188
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-04-09 18:08:24 +0530

    url-parameters

--- Web Development with Python/webserver/server.py
+++ Web Development with Python/webserver/server.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 print(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('./index.html')
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+    return render_template('./index.html', name=username, post_id=post_id)
 
 
 @app.route('/blog')
