f61b106437087fc93d16814da17d6916fa5ba5d3
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-04-14 00:07:53 +0530

    portfolio-3

--- Web Development with Python/Portfolio-Website/server.py
+++ Web Development with Python/Portfolio-Website/server.py
@@ -4,36 +4,11 @@ app = Flask(__name__)
 print(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template('./index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route('/about.html')
-def about():
-    return render_template('./about.html')
-
-
-@app.route('/components.html')
-def components():
-    return render_template('./components.html')
-
-
-@app.route('/thankyou.html')
-def thankyou():
-    return render_template('./thankyou.html')
-
-
-@app.route('/work.html')
-def work():
-    return render_template('./work.html')
-
-
-@app.route('/works.html')
-def works():
-    return render_template('./works.html')
-
-
-@app.route('/contact.html')
-def contact():
-    return render_template('./contact.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'Form submitted hoooraaayyyy'
