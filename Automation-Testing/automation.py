8dd85d20b5a25129354de1e117ac4d6f765fe949
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-04-28 21:11:23 +0530

    automation-3

--- Automation-Testing/automation.py
+++ Automation-Testing/automation.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from time import sleep
+from selenium.webdriver.common.by import By
 
-ser = Service(r'./chromedriver.exe')
+ser = Service(executable_path='./chromedriver.exe')
 ops = webdriver.ChromeOptions()
 chrome_browser = webdriver.Chrome(service=ser, options=ops)
-chrome_browser.get('https://www.google.com/')
 chrome_browser.maximize_window()
-print('maximised successfully')
+chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
+
+assert 'Selenium Easy Demo ' in chrome_browser.title
+button_text = chrome_browser.find_element(By.CLASS_NAME, 'btn-primary')
+print(button_text.get_attribute('innerHTML'))
+
+sleep(20)
