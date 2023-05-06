f5a8f46e966a24af13fefc8be0ff598d20d0c0b9
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-05-06 14:15:55 +0530

    EndOfModule

--- Automation-Testing/automation.py
+++ Automation-Testing/automation.py
@@ -10,7 +10,22 @@ chrome_browser.maximize_window()
 chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
 
 assert 'Selenium Easy Demo ' in chrome_browser.title
-button_text = chrome_browser.find_element(By.CLASS_NAME, 'btn-primary')
-print(button_text.get_attribute('innerHTML'))
+show_message_button = chrome_browser.find_element(By.CLASS_NAME, 'btn-primary')
+print(show_message_button.get_attribute('innerHTML'))
 
-sleep(20)
+assert 'Show Message' in chrome_browser.page_source
+user_message = chrome_browser.find_element(By.ID, "user-message")
+user_message.clear()
+user_message.send_keys('I am very cool mate')  # Enter message buttons
+
+show_message_button.click()
+
+output_message = chrome_browser.find_element(By.ID, "display")
+
+assert 'I am very cool mate' == output_message.text
+print('assertion complete')
+
+user_buttons2 = chrome_browser.find_element(By.CSS_SELECTOR, '#get-input > .btn')
+print(user_buttons2)
+
+sleep(30)
