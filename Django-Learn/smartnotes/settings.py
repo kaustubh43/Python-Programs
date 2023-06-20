b09c0325735381f9e01efaf8d682500ea1201955
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-06-20 22:26:10 +0530

    Django-7

--- Django-Learn/smartnotes/settings.py
+++ Django-Learn/smartnotes/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'smartnotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            BASE_DIR / 'static/templates',
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'notes/static'), ]
 
