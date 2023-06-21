26817d6e45149aa601138cdc0fd2926a6b65f856
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-06-21 21:02:52 +0530

    Django-8

--- Django-Learn/notes/urls.py
+++ Django-Learn/notes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NoteListView.as_view()),
-    path('notes/<int:pk>', views.NoteDetailView.as_view()),
+    path('notes', views.NoteListView.as_view(), name='notes.list'),
+    path('notes/<int:pk>', views.NoteDetailView.as_view(),name='notes.details'),
     path('NotFound', views.NotFoundView.as_view(), name='NotFound')
 ]
