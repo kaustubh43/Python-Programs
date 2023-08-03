7fc5d851d2884356fc368485cf5e21bc71d2cc2e
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-08-03 20:58:45 +0530

    pytest 2

--- Testing in Python/Unit Testing using Pytest/scripts/chp2/video2/mapmaker_start.py
+++ Testing in Python/Unit Testing using Pytest/scripts/chp2/video2/mapmaker_start.py
@@ -1,2 +1,11 @@
 class Point():
-    pass
+    """Coords for cities, Latitude and Longitude"""
+
+    def __init__(self, name, lat, long) -> None:
+        self._latitude = lat
+        self._longitude = long
+        self._name = name
+
+    def get_lat_long(self):
+        """returns latitude and longitude"""
+        return self._latitude, self._longitude
