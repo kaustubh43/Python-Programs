d54b47942da32371877a76ccda6bd6e5a8223f2d
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-08-07 21:50:49 +0530

    pytest-3

--- Testing in Python/Unit Testing using Pytest/scripts/chp2/video2/mapmaker_start.py
+++ Testing in Python/Unit Testing using Pytest/scripts/chp2/video2/mapmaker_start.py
@@ -2,6 +2,10 @@ class Point():
     """Coords for cities, Latitude and Longitude"""
 
     def __init__(self, name, lat, long) -> None:
+        if not (-90 <= lat <= 90) or not (-180 <= long <= 180):
+            raise ValueError("Invalid latitude and longitude")
+        if not name.isalpha():
+            raise ValueError("Name should contain only alphabets")
         self._latitude = lat
         self._longitude = long
         self._name = name
--- Testing in Python/Unit Testing using Pytest/tests/chp2/video2/test_mapmaker_start.py
+++ Testing in Python/Unit Testing using Pytest/tests/chp2/video2/test_mapmaker_start.py
@@ -1,6 +1,19 @@
 from scripts.chp2.video2.mapmaker_start import Point
+import pytest
 
 
 def test_make_one_point():
     p1 = Point('Dakar', 14.7167, 17.4676)
     assert p1.get_lat_long() == (14.7167, 17.4676)
+
+
+def test_invalid_point_generation():
+    with pytest.raises(ValueError) as exp:
+        Point("New York", 120.678120, -555.01238)
+    assert str(exp.value) == 'Invalid latitude and longitude'
+
+
+def test_invalid_city_name():
+    with pytest.raises(Exception) as exp:
+        Point("$anFrancisco", 37.6192, 122.3816)
+    assert str(exp.value) == 'Name should contain only alphabets'
