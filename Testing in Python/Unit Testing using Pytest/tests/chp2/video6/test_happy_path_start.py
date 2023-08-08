4de4795f6d9073ebdf9c3890f146552801817cb0
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-08-08 21:58:36 +0530

    pytest-4

--- Testing in Python/Unit Testing using Pytest/tests/chp2/video6/test_happy_path_start.py
+++ Testing in Python/Unit Testing using Pytest/tests/chp2/video6/test_happy_path_start.py
@@ -12,13 +12,13 @@ def test_csv_reader_header_fields(process_data):
     data = process_data
     header_fields = list(data[0].keys())
     assert header_fields == [
-            'Country',
-            'City',
-            'State_Or_Province',
-            'Lat',
-            'Long',
-            'Altitude'
-            ]
+        'Country',
+        'City',
+        'State_Or_Province',
+        'Lat',
+        'Long',
+        'Altitude'
+    ]
 
 
 def test_csv_reader_data_contents(process_data):
@@ -26,9 +26,21 @@ def test_csv_reader_data_contents(process_data):
     Happy Path Test to examine that each row
     has the appropriate data type per field
     """
-    # data = process_data
+    data = process_data
 
     # Check row types
+    for row in data:
+        # operations
+        assert (isinstance(row['Country'], str))
+        assert (isinstance(row['City'], str))
+        assert (isinstance(row['State_Or_Province'], str))
+        assert (isinstance(row['Lat'], float))
+        assert (isinstance(row['Long'], float))
+        assert (isinstance(row['Altitude'], float))
+
+    assert len(data) == 180
+    assert data[0]['Country'] == 'Andorra'
+    assert data[179]['Country'] == 'United States'
 
     # Basic data checks
 
