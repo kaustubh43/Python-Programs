ee8a689e63b66a2c41cbd6a4975db3191f3e260b
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-08-29 21:52:00 +0530

    RESTFUL API completed

--- RESTFUL-APIs/app.py
+++ RESTFUL-APIs/app.py
@@ -149,11 +149,12 @@ def login():
         return jsonify(message="Bad email or password"), 401
 
 
-@app.route('/reset_password/<string:email>',methods=['GET'])
-def reset_password(email:str):
+@app.route('/reset_password/<string:email>', methods=['GET'])
+def reset_password(email: str):
     user = User.query.filter_by(email=email).first()
     if user:
-        msg = Message(f'Your planetary API password is {user.password}',
+        msg = Message(subject=f'Forgot your planetary api password?',
+                      body=f'Your password is {user.password}',
                       sender='planetary-api@noreply.com',
                       recipients=[email])
         mail.send(msg)
@@ -161,6 +162,69 @@ def reset_password(email:str):
     else:
         return jsonify(message='That email does not exist'), 401
 
+
+@app.route('/planet_details/<int:planet_id>', methods=['GET'])
+def planet_details(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        result = planet_schema.dump(planet)
+        return jsonify(result)
+    else:
+        return jsonify(message='That planet does not exist'), 404
+
+
+@app.route('/add_planet', methods=['POST'])
+@jwt_required()
+def add_planet():
+    planet_name = request.form['planet_name']
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify(message='There is already a planet by that name'), 409
+    else:
+        planet_type = request.form['planet_type']
+        home_star = request.form['home_star']
+        mass = float(request.form['mass'])
+        radius = float(request.form['radius'])
+        distance = float(request.form['distance'])
+
+        new_planet = Planet(planet_name=planet_name,
+                            planet_type=planet_type,
+                            home_star=home_star,
+                            mass=mass,
+                            radius=radius,
+                            distance=distance)
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(message=f'You added {planet_name} in your db'), 201
+
+
+@app.route('/update_planet', methods=['POST'])
+@jwt_required()
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        planet.planet_name = request.form['planet_name']
+        planet.planet_type = request.form['planet_type']
+        planet.home_star = request.form['home_star']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message=f"You updated a planet {request.form['planet_name']}"), 202
+    else:
+        return jsonify(message='Planet you are trying to update does not exist'), 404
+
+
+@app.route('/remove_planet/<int:planet_id>', methods=['DELETE'])
+def remove_planet(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message=f'You have deleted {planet.planet_name}')
+    else:
+        return jsonify(message='Planet that you requested is not found')
 
 # Database models
 class User(db.Model):
