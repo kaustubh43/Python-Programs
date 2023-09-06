67d42297b6d6df387d790ebd6bb3626ed04b5c4d
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-09-06 21:27:31 +0530

    Django REST

--- Django-REST/store/api_views.py
+++ Django-REST/store/api_views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 
 from store.serializers import ProductSerializer
 from store.models import Product
@@ -7,3 +8,20 @@ from store.models import Product
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('id', 'name', 'price')
+
+    def get_queryset(self):
+        on_sale = self.request.query_params.get('on_sale', None)
+        if on_sale is None:
+            return super().get_queryset()
+        queryset = Product.objects.all()
+        if on_sale.lower() == 'true':
+            from django.utils import timezone
+            now = timezone.now()
+            return queryset.filter(
+                sale_start__lte=now,
+                sale_end__gte=now,
+            )
+        return queryset
+
